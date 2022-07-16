e84166157052d85fc88169b6a4af82200fa77787
Armando Capita <91953802+Armandomateus41@users.noreply.github.com>
2022-07-15 22:38:35 -0300

    Create Desafionext.js

---
+++ Desafionext.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Menu from '../componente/Menu';
+function Home() 
+{
+    return 
+    <div>
+        <Head>
+           <meta charSet="utf-8" />
+           <meta name="robtos"  content="index, follow" />
+           <meta name= "description " content=" Desafio..." />
+           <meta name= "author" content=" Desafio- Chris" />
+           <meta name= "viewport"  content=" initial-scale=1.0,
+           width=devide-width" />
+           <title> desafio- Home</title>
+
+
+
+
+        </Head>
+        <br />
+    <link href="/">
+   <a style={linkStyle}>Home</a>
+    </link>
+
+    <link href="/">
+   <a style={linkStyle}>Pesquisas</a>
+
+    </link>
+
+    <link href="/">
+   <a style={linkStyle}>Cadastro</a>
+    </link>
+
+        </div>
+  }
+  
+  export default Home;
